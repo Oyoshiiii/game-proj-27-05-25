c5304e031c92e2dde233ed596d516ef6b7537a73
Casey Tyler <keshakeshievich@gmail.com>
2025-05-28 19:32:55 +0700

    изменены картинки для pilotCabin, добавлена модель Item для элементов страницы

--- game proj 27 05 25/Controllers/PilotCabinController.cs
+++ game proj 27 05 25/Controllers/PilotCabinController.cs
@@ -1,6 +1,17 @@
-﻿namespace game_proj_27_05_25.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using game_proj_27_05_25.Models;
+
+namespace game_proj_27_05_25.Controllers
 {
-    public class PilotCabinController
+    public class PilotCabinController : Controller
     {
+        private const string SessionKey = "PilotCabinItems";
+        [Route("/PilotCabin")]
+        [HttpGet]
+        public IActionResult PilotCabin()
+        {
+            
+            return View();
+        }
     }
 }
---
+++ game proj 27 05 25/Models/Item.cs
@@ -0,0 +1,19 @@
+﻿namespace game_proj_27_05_25.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "";
+        public string ImagePath { get; set; } = "";
+        public int X { get; set; }
+        public int Y { get; set; }
+        public InteractionType Interaction { get; set; } 
+        public bool IsInteracted { get; set; }
+    }
+
+    public enum InteractionType
+    {
+        SimpleAction, //просто кликабельные объекты
+        PickupDialog  //объекты с диалоговым окном, которые можно поднять
+    }
+}
